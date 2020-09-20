4f5401a1c0ad6f235b7f432a12924bf052cc7be7
kazemicode <sarieses@gmail.com>
2020-09-20 04:57:03 -0700

    Flixter -- Added API access and Movie class

---
+++ app/src/main/java/org/kazemicode/flixter/models/Movie.java
@@ -0,0 +1,40 @@
+package org.kazemicode.flixter.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Movie {
+    private String posterPath;
+    private String title;
+    private String overview;
+
+    public Movie(JSONObject jsonObject) throws JSONException {
+        posterPath = jsonObject.getString("poster_path");
+        title = jsonObject.getString("title");
+        overview = jsonObject.getString("overview");
+    }
+
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
+        List<Movie> movies = new ArrayList<>();
+        for(int i = 0; i < movieJsonArray.length(); i++){
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+        }
+        return movies;
+    }
+
+    /* Getters */
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+}
